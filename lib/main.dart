168add1335d08f4b63158b2b829c353f2a2d7889
icestar <csg@qq.com>
2022-01-14 15:25:32 +0800

    fix: catch error

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eth_wallet/modules/sp_utils.dart';
 import 'package:flutter_eth_wallet/pages/wallet_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> main() async {
-  SpUtils.sp = await SharedPreferences.getInstance();
-  runApp(const MyApp());
+void main() async {
+  runZonedGuarded<Future<void>>(() async{
+    SpUtils.sp = await SharedPreferences.getInstance();
+    runApp(const MyApp());
+  } , (obj, stackTrace) {
+    try {
+      if (!kReleaseMode ) {
+      }
+    } catch (_) {}
+  });
 }
