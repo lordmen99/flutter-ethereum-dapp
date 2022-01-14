b928893e3356749ed75436edace7dd78524cb11a
icestar <csg@qq.com>
2022-01-14 15:34:32 +0800

    fix: bloc unInit

---
+++ lib/blocs/bloc_observer.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+const String _TAG = "AppBlocObserver";
+
+class AppBlocObserver extends BlocObserver {
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_eth_wallet/blocs/bloc_observer.dart';
 import 'package:flutter_eth_wallet/modules/sp_utils.dart';
 import 'package:flutter_eth_wallet/pages/wallet_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   runZonedGuarded<Future<void>>(() async{
+    WidgetsFlutterBinding.ensureInitialized();
     SpUtils.sp = await SharedPreferences.getInstance();
+    Bloc.observer = AppBlocObserver();
     runApp(const MyApp());
   } , (obj, stackTrace) {
     try {
